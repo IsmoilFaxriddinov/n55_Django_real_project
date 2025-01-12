2cbdf600127f81f1b7e75045326b625e9dc8ea0e
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-12 21:11:09 +0500

    a

--- conf/urls.py
+++ conf/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls', namespace='pages'))
+    path('', include('pages.urls', namespace='pages')),
 ]
 
 
