913e942651558c82afbff0cd70ce02156f74847f
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-14 15:14:08 +0500

    a

--- conf/settings.py
+++ conf/settings.py
@@ -107,9 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = (
+    ('en', 'English'),
+    ('uz', "O'zbek")
+)
+
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
