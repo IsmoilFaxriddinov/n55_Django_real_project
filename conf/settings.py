4d32697717f19a94a72a59b24d130705071269cc
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-15 19:58:35 +0500

    a

--- conf/settings.py
+++ conf/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -113,7 +113,7 @@ LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
     ('en', 'English'),
-    ('uz', "O'zbek")
+    ('uz', "O'zbek"),
 )
 
 LOCALE_PATHS = (BASE_DIR / 'locale',)
