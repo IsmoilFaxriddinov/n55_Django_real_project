07dfda226e43d63ab2cff5f34726d02185826cf7
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-15 18:54:44 +0500

    a

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='users/login')
 class HomeTemplateView(TemplateView):
     template_name = 'pages/home.html'
 
