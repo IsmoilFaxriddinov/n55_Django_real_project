3de3f1da8f21b0533b4a203c481c768cadbff718
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-20 22:04:23 +0500

    a

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from blogs.views import BlogListView
+from blogs.views import BlogDetailView, BlogListView
 
 app_name="blogs"
 
 urlpatterns = [
-    path('blogs/', BlogListView.as_view(), name='list')
+    path('blogs/', BlogListView.as_view(), name='list'),
+    path('blogs/<int:pk>', BlogDetailView.as_view(), name='detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blogs.models import BlogCategoryModel, BlogModel, BlogTagModel
 
 class BlogListView(ListView):
@@ -23,5 +23,21 @@ class BlogListView(ListView):
         context["categories"] = BlogCategoryModel.objects.all()
         context["recent_blogs"] = BlogModel.objects.order_by('-created_at')[:2]
         context["tags"] = BlogTagModel.objects.all()
+        return context
+    
+
+class BlogDetailView(DetailView):
+    model = BlogModel
+    template_name = 'blogs/blog-detail.html'
+    context_object_name = 'blog'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blog = self.object
+        categories_id = blog.categories.values_list('id', flat=True)
+
+        related_blogs = BlogModel.objects.filter(categories__in=categories_id).exclude(id=blog.id).distinct()[:3]
+
+        context['related_blogs'] = related_blogs
         return context
     
