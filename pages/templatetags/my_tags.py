e78821159d54bdc0da88582702d4ae1c3a975166
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-20 18:25:11 +0500

    a

--- pages/templatetags/my_tags.py
+++ pages/templatetags/my_tags.py
@@ -7,3 +7,12 @@ def get_lang_url(request, lang):
     url = request.path.split('/')
     url[1] = lang
     return '/'.join(url)
+
+@register.simple_tag
+def get_lang_flag(lang):
+    if lang == 'uz':
+        return 'uz'
+    elif lang == 'en':
+        return 'en'
+    else:
+        return 'ru'
