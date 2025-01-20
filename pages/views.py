995ad9084a7acfd2f738e86c0d61081a79a1b1bc
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-20 21:17:01 +0500

    q

--- pages/views.py
+++ pages/views.py
@@ -10,15 +10,20 @@ class BlogListTemplateView(ListView):
     template_name = 'blogs/blog-list.html'
     context_object_name = 'blogs'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self):
+        
         tag = self.request.GET.get('tag')
         blogs = BlogModel.objects.all()
-        if tag:
-            blogs = blogs.filter()
-
         category = self.request.GET.get('category')
-        context["blogs"] = blogs
+        if tag:
+            blogs = blogs.filter(tags=tag)
+        elif category:
+            blogs = blogs.filter(categories=category)
+        return blogs
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["categories"] = BlogCategoryModel.objects.all()
         context["recent_blogs"] = BlogModel.objects.order_by('-created_at')[:2]
         context["tags"] = BlogTagModel.objects.all()
