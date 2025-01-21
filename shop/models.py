3cd5787765b492c826756952d2d8ccba096104d4
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-21 19:57:57 +0500

    a

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,51 +9,51 @@ class BaseModel(models.Model):
         abstract = True
 
 class ColorModel(BaseModel):
-    heh_code = models.CharField(max_length=125)
-    color_name = models.CharField(max_length=125)
+    code = models.CharField(max_length=125, verbose_name=_('code'))
+    title = models.CharField(max_length=125, verbose_name=_('title'))
 
     def __str__(self):
         return self.color_name
     
     class Meta:
-        verbose_name = 'color'
-        verbose_name_plural = 'colors'
+        verbose_name = _('color')
+        verbose_name_plural = _('colors')
 
 class ProductModel(BaseModel):
-    image = models.ImageField(upload_to='products/')
+    image1 = models.ImageField(upload_to='products/')
     image2 = models.ImageField(upload_to='products/')
-    title = models.CharField(max_length=125)
-    color = models.ForeignKey(ColorModel, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=125, verbose_name=_('title'))
+    color = models.ForeignKey(ColorModel, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('color'))
     price = models.FloatField()
-    description = models.TextField
+    description = models.TextField(verbose_name=_('description'))
 
     def __str__(self):
         return self.title
     
     class Meta:
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
 
 class ProductCategoryModel(BaseModel):
-    title = models.CharField(max_length=125, verbose_name=('title'))
-    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True, related_name='children', verbose_name=('parent'))
+    title = models.CharField(max_length=125, verbose_name=_('title'))
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True, related_name='children', verbose_name=_('parent'))
 
     def __str__(self):
         return self.title
     
     class Meta:
-        verbose_name = ('blog category')
-        verbose_name_plural = ('blog cetegories')
+        verbose_name = _('blog category')
+        verbose_name_plural = _('blog cetegories')
 
 class TagModel(BaseModel):
-    name = models.CharField(max_length=125)
+    name = models.CharField(max_length=125, verbose_name=_('name'))
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'tag'
-        verbose_name_plural = 'tags'
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
     
 # class PriceModel(BaseModel):
 #     price = models.FloatField()
