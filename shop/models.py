d200982de59c5d702600dc115db1bd02ff3bf706
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-22 22:48:37 +0500

    a

--- shop/models.py
+++ shop/models.py
@@ -1,38 +1,18 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
-    class Meta:
-        abstract = True
+from app_common.models import BaseModel
 
 class ColorModel(BaseModel):
     code = models.CharField(max_length=125, verbose_name=_('code'))
     title = models.CharField(max_length=125, verbose_name=_('title'))
 
     def __str__(self):
-        return self.color_name
+        return self.title
     
     class Meta:
         verbose_name = _('color')
         verbose_name_plural = _('colors')
 
-class ProductModel(BaseModel):
-    image1 = models.ImageField(upload_to='products/')
-    image2 = models.ImageField(upload_to='products/')
-    title = models.CharField(max_length=125, verbose_name=_('title'))
-    color = models.ForeignKey(ColorModel, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('color'))
-    price = models.FloatField()
-    description = models.TextField(verbose_name=_('description'))
-
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        verbose_name = _('Product')
-        verbose_name_plural = _('Products')
 
 class ProductCategoryModel(BaseModel):
     title = models.CharField(max_length=125, verbose_name=_('title'))
@@ -55,12 +35,36 @@ class TagModel(BaseModel):
         verbose_name = _('tag')
         verbose_name_plural = _('tags')
     
-# class PriceModel(BaseModel):
-#     price = models.FloatField()
+class ProductSizeModel(BaseModel):
+    name = models.CharField(max_length=125, verbose_name=_('name'))
 
-#     def __str__(self):
-#         return self.price
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('size')
+        verbose_name_plural = _('sizes')
     
-#     class Meta:
-#         verbose_name = 'price'
-#         verbose_name_plural = 'prices'
+class ProductModel(BaseModel):
+    image1 = models.ImageField(upload_to='products/')
+    image2 = models.ImageField(upload_to='products/')
+    title = models.CharField(max_length=125, verbose_name=_('title'))
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(verbose_name=_('description'))
+    sku = models.CharField(max_length=125)
+
+    color = models.ManyToManyField(ColorModel, related_name='colors', verbose_name=_('color'))
+    tags = models.ManyToManyField(TagModel, related_name='tags', verbose_name=_('tags'))
+    categories = models.ManyToManyField(ProductCategoryModel, related_name='categories', verbose_name=_('categories'))
+    sizes = models.ManyToManyField(ProductSizeModel, related_name='sizes', verbose_name=_('size'))
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+class ProductImageModel(models.Model):
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/images/')
