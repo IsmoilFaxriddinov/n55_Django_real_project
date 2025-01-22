60536b5bedc30fa127626593df6d8a229f00991e
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-22 21:17:38 +0500

    a

--- pages/models.py
+++ pages/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+from app_common.models import BaseModel
+
+class ContactModel(BaseModel):
+    name = models.CharField(max_length=125, verbose_name=_('name'))
+    email = models.EmailField(verbose_name=_('email'))
+    subject = models.CharField(max_length=125, verbose_name=_('subject'))
+    message = models.TextField(verbose_name=_('message'))
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = _('Contact')
+        verbose_name_plural = _('Contacts')
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.decorators import login_required
+
+from pages.models import ContactModel
 
 class HomeTemplateView(TemplateView):
     template_name = 'pages/home.html'
 
-class ContactTemplateVIew(TemplateView):
+class ContactTemplateVIew(CreateView):
     template_name = 'pages/contact.html'
+    form_class = ContactModelForm
+    model = ContactModel
+
+    # def form_valid(self, form):
+    #     form
+    #     return super().form_valid(form)
+    
+    # def form_invalid(self, form):
+    #     response = super().form_invalid(form)
+    #     response
+    
