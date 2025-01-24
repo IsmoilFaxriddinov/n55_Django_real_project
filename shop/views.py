7e9747ef133a441f1c6cfacee721e4be95e04241
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-24 21:57:50 +0500

    a

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,22 @@
+from django.db.models import Q
 from django.views.generic import ListView, TemplateView
 
 from shop.models import *
 
 class ProductTemplateView(ListView):
     template_name = 'shop/products-list.html'
-    model = ProductModel
     context_object_name = 'products'
-    paginate_by = 2
+    paginate_by = 1
+
+    def get_queryset(self):
+        products = ProductModel.objects.all()
+        q = self.request.GET.get('q')
+        if q:
+            products = products.filter(
+                Q(title_icontains=q) | Q(short_description_icontains=q) | Q(long_description)
+            )
+        return super().get_queryset()
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
