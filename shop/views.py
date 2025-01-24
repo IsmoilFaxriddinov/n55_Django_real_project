ff3a73a901b16eb763d65df9e9b43b59206dbf74
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-24 20:51:28 +0500

    a

--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,7 @@ from shop.models import ProductModel
 class ProductTemplateView(ListView):
     template_name = 'shop/products-list.html'
     model = ProductModel
+    context_object_name = 'products'
 
 class ProductDetailTemplateView(TemplateView):
     template_name = 'shop/product-detail.html'
