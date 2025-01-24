c913f1759a24f713c904dd2d63f76a587e00f9df
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-24 21:04:03 +0500

    a;

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,20 @@
 from django.views.generic import ListView, TemplateView
 
-from shop.models import ProductModel
+from shop.models import *
 
 class ProductTemplateView(ListView):
     template_name = 'shop/products-list.html'
     model = ProductModel
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sizes"] = ProductSizeModel
+        context["colors"] = ProductColorModel
+        context["categories"] = ProductCategoryModel
+        context["tags"] = ProductTagModel
+        return context
+    
 
 class ProductDetailTemplateView(TemplateView):
     template_name = 'shop/product-detail.html'
