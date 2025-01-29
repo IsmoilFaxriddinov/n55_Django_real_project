7743fc621b00bfbadb38169d0484ebe17d3a2c33
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-29 21:56:29 +0500

    commit

--- orders/templatetags/order_tag.py
+++ orders/templatetags/order_tag.py
@@ -1,7 +1,18 @@
 from django import template
 
+from shop.models import ProductModel
+
 register = template.Library()
 
 @register.filter
 def in_cart(request, pk):
-    return pk in request.session.get('get', [])
+    return pk in request.session.get('cart', [])
+
+@register.filter
+def get_user_cart(request):
+    cart = request.session.get('cart', [])
+    products = []
+    for pk in cart:
+        product = ProductModel.objects.filter(pk=pk)
+        products.append(product)
+    return products        
