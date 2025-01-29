cb545de319e1bde2fea1725c15b58e3117d278ad
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-29 21:15:52 +0500

    commit

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def add_or_remove_cart(request, pk):
-    ...
+    cart: list = request.session.get('cart', [])
+    if pk in cart:
+        cart.remove(pk)
+    else:
+        cart.append(pk)
+
+    request.session['cart'] = cart
+    return redirect()
 
 def add_or_remove_wishlist(request, pk):
     ...
