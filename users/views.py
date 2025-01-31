11c2d6e7e0eac6f949bc31de3a67aabd0fbeba4e
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-31 22:34:32 +0500

    commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import FormView
 
+from users.forms import RegisterForm
 
+class RegisterView(FormView):
+    template_name = 'auth/user-register.html'
+    form_class = RegisterForm
+    success_url = '/'
+    
