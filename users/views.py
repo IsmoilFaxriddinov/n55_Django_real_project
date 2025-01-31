b869a9cda894b285813ff935ae410512a74b911e
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-31 23:05:27 +0500

    commit

--- users/views.py
+++ users/views.py
@@ -7,4 +7,11 @@ class RegisterView(FormView):
     template_name = 'auth/user-register.html'
     form_class = RegisterForm
     success_url = '/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+        
     
