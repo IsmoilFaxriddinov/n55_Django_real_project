b77ac03707772e478e5b7bbd7bad047e2144a15a
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-02 14:07:28 +0500

    commit

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     # path("update/password/", RegisterView.as_view(), name="register"),
     # path("forget/password/", RegisterView.as_view(), name="register"),
     # path("verification/recent/", RegisterView.as_view(), name="register"),
-    path("verification/", confirm_email, name="confirm-email"),
+    path("verification/<int:uid>/<str:token>/", confirm_email, name="confirm-email"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
+from django.views.generic import View
 from django.views.generic import FormView, CreateView
 
 from users.forms import RegisterForm
@@ -10,7 +11,7 @@ from users.utils import send_email_confirmation
 
 UserModel = get_user_model()
 
-class RegisterView(CreateView):
+class RegisterView(FormView):
     template_name = 'auth/user-register.html'
     form_class = RegisterForm
     success_url = '/'
@@ -46,3 +47,19 @@ def confirm_email(request, uid, token):
         messages.success(request, 'Link is not correnct')
         return redirect('/')
         
+
+class VerifyEmail(View):
+    def get(self, uid, token):
+        try:
+            user = UserModel.objects.get(id=uid)
+        except UserModel.DoesNotExist:
+            return redirect('users:login')
+        
+        if default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            messages.success(self.request, 'Your email address id verified')
+            return redirect('/')
+        else:
+            messages.success(self.request, 'Link is not correnct')
+            return redirect('/')
