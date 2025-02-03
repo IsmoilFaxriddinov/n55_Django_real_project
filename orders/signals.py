70953426deb01bfcd48cc6121bfbd7bdf4ec9e74
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-03 21:11:08 +0500

    commit

--- orders/signals.py
+++ orders/signals.py
@@ -5,7 +5,7 @@ from shop.models import ProductModel
 
 @receiver(pre_save, sender=ProductModel)
 def update_product_price(sender, instance, **kwargs):
-    if instance.discount:
+    if instance.discount and instance.discount > 0:
         instance.discount_price = instance.price - (instance.price * instance.discount / 100)
     else:
         instance.discount_price = instance.price
