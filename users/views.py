b8991d07ca7e0029280a78438bc18ea87e84e872
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-03 17:05:32 +0500

    commit

--- users/views.py
+++ users/views.py
@@ -91,6 +91,7 @@ class UpdatePasswordView(FormView):
         return super().form_valid(form)
     
     def form_invalid(self, form):
+        print(form.errors)
         return super().form_invalid(form)
     
     
